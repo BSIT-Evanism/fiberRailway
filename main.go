c0012c1a71b618f5379df88a8abaf5001e510d04
BSIT-Evanism <johnevanporcioncula.solanoy@bicol-u.edu.ph>
2023-10-14 20:43:38 +0800

    add

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func getPort() string {
@@ -19,6 +20,7 @@ func getPort() string {
 
 func main() {
 	app := fiber.New()
+	app.Use(cors.New())
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{
